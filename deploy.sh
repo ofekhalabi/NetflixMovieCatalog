33f4eeec75d10c113717e409feeef8c4b67bd6f5
ofekh <halabi1205@gmail.com>
2024-08-27 20:59:19 +0300

    20

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,7 @@
 sudo  systemctl daemon-reload
 sudo systemctl stop Netflix_instance_UP.service
 sudo systemctl start Netflix_instance_UP.service
+if [ -e /home/ubuntu/app/mykey.pem ]; then
+  rm /home/ubuntu/app/mykey.pem
+fi
 
