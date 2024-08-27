dc7fae502a0db85622190a6de70ae7ccd196fd1b
ofekh <halabi1205@gmail.com>
2024-08-27 20:25:00 +0300

    update workflow_13

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ with open('data/data_movies.json', 'r') as f:
 
 @app.route("/", methods=['GET'])
 def home():
-    return "Hi! This app is an API, there is no UI ;)"
+    return "Hi! This app is an API, there is no UI ;)hay balolo met hayom"
 
 
 @app.route('/discover')
